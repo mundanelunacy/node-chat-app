8a54db20d66bdb914dc11fdc26f47767b5744a74
Satoshi Kawase <kawase@jhu.edu>
2018-12-12 16:22:47 +0900

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket){
 	console.log('new user connected');
 
+	//socket.emit from Admin text Welcom to the chat app
+	socket.emit('newMessage', {
+		from: "Admin",
+		text: "Welcom to the chat",
+		createdAt : new Date().getTime()
+	});
+
+	//socket.broadcast.emit from Admin text New user joined
+	socket.broadcast.emit('newMessage', {
+		from : "Admin",
+		text : "New user joined",
+		createdAt : new Date().getTime()
+	});
+
 	socket.on('createMessage', function(message){
 		console.log('createMessage', message);
 		io.emit('newMessage', {
@@ -22,6 +36,12 @@ io.on('connection', function(socket){
 			text: message.text,
 			createdAt : new Date().getTime()
 		});
+
+		// socket.broadcast.emit('newMessage', {
+		// 		from: message.from,
+		// 		text: message.text,
+		// 		createdAt : new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', function(){
